39c5e53cc482e3a97b3fdfc586d610600fec562d
xrfang <xrfang@gmail.com>
2020-04-27 07:14:39 +0800

    WIP: initialize db

--- src/db/init.go
+++ src/db/init.go
@@ -7,11 +7,50 @@ import (
 
 func setupSchema() {
 	db.MustExec(`CREATE TABLE IF NOT EXISTS "user" (
-		"id"	 INTEGER PRIMARY KEY AUTOINCREMENT,
-		"name"	 TEXT NOT NULL,
-		"login"	 TEXT NOT NULL UNIQUE,
-		"otpkey" TEXT NOT NULL DEFAULT ""
-	)`)
+        "id"     INTEGER PRIMARY KEY AUTOINCREMENT,
+        "name"   TEXT NOT NULL,
+        "login"  TEXT NOT NULL UNIQUE,
+        "otpkey" TEXT NOT NULL DEFAULT ""
+    )`)
+	db.MustExec(`CREATE TABLE IF NOT EXISTS "herb" (
+        "id"     INTEGER PRIMARY KEY AUTOINCREMENT,
+        "name"   TEXT NOT NULL UNIQUE,
+        "pinyin" TEXT NOT NULL DEFAULT "",
+        "alias"  INTEGER NOT NULL DEFAULT 0,
+        "stock"  INTEGER NOT NULL DEFAULT 0,
+        "unit"   TEXT NOT NULL,
+        "cost"   NUMERIC NOT NULL DEFAULT 0
+    )`)
+	db.MustExec(`CREATE TABLE IF NOT EXISTS "sku" (
+        "caption" TEXT NOT NULL,
+        "base"    TEXT NOT NULL DEFAULT "",
+        "count"   INTEGER NOT NULL DEFAULT 1,
+        PRIMARY KEY("caption")
+    ) WITHOUT ROWID`)
+	db.MustExec(`CREATE TABLE IF NOT EXISTS "bom_type" (
+        "id"      INTEGER,
+        "caption" TEXT NOT NULL UNIQUE,
+        "class"   INTEGER NOT NULL,
+        PRIMARY KEY("id")
+    ) WITHOUT ROWID`)
+	db.MustExec(`CREATE TABLE IF NOT EXISTS "bom" (
+        "id"      INTEGER PRIMARY KEY AUTOINCREMENT,
+        "type"    INTEGER NOT NULL,
+        "user_id" INTEGER NOT NULL,
+        "money"   NUMERIC NOT NULL DEFAULT 0,
+        "memo"    TEXT NOT NULL DEFAULT "",
+        "status"  INTEGER NOT NULL DEFAULT 0,
+        "created" INTEGER NOT NULL,
+        "updated" INTEGER NOT NULL
+    )`)
+	db.MustExec(`CREATE TABLE IF NOT EXISTS "bom_item" (
+        "id"      INTEGER PRIMARY KEY AUTOINCREMENT,
+        "bom_id"  INTEGER NOT NULL,
+        "herb_id" INTEGER NOT NULL,
+        "unit"    TEXT NOT NULL,
+        "count"   INTEGER NOT NULL,
+        "status"  INTEGER NOT NULL
+    )`)
 }
 
 func setupParams() {

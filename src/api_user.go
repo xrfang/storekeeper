aae70fc8f04e1bd94e48a87cff146ea806c04871
xrfang <xrfang@gmail.com>
2020-04-30 17:42:15 +0800

    WIP: user management UI

--- src/api_user.go
+++ src/api_user.go
@@ -8,16 +8,23 @@ import (
 )
 
 func apiUsers(w http.ResponseWriter, r *http.Request) {
-	if !validate(r) {
-		http.Error(w, "Unauthorized", http.StatusUnauthorized)
-		return
-	}
+	// if !validate(r) {
+	// http.Error(w, "Unauthorized", http.StatusUnauthorized)
+	// return
+	// }
 	switch r.Method {
 	case "GET":
 		users, err := db.ListUsers(1)
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
+		}
+		for i := 0; i < 20; i++ {
+			u := users[0]
+			if i%2 != 0 {
+				u.Client = 1
+			}
+			users = append(users, u)
 		}
 		w.Header().Set("Content-Type", "application/json")
 		json.NewEncoder(w).Encode(users)
--- src/h_users.go
+++ src/h_users.go
@@ -5,9 +5,9 @@ import (
 )
 
 func users(w http.ResponseWriter, r *http.Request) {
-	if !validate(r) {
-		http.Redirect(w, r, "/login", http.StatusTemporaryRedirect)
-		return
-	}
+	// if !validate(r) {
+	// http.Redirect(w, r, "/login", http.StatusTemporaryRedirect)
+	// return
+	// }
 	renderTemplate(w, "users.html", nil)
 }

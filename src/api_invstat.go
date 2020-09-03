fc4d15757f798e4e8e361544c9cc3c605657f198
xiangrong.fang <xiangrong.fang@sensethink.com>
2020-09-03 09:19:51 +0800

    WIP: 采购建议

--- src/api_invstat.go
+++ src/api_invstat.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 	"storekeeper/db"
 )
@@ -13,21 +12,13 @@ func apiInvStat(w http.ResponseWriter, r *http.Request) {
 		}
 	}()
 	uid := db.CheckToken(getCookie(r, "token"))
-	uid = 1 //DEBUG ONLY
 	if uid == 0 {
 		http.Error(w, "Unauthorized", http.StatusUnauthorized)
 		return
 	}
 	inuse, unuse := db.AnalyzeGoodsUsage()
-	fmt.Fprintln(w, "建议采购：")
-	for _, g := range inuse {
-		fmt.Fprintf(w, "%s %v克 ", g.Name, g.Amount)
-	}
-	fmt.Fprintln(w)
-	fmt.Fprintln(w, "===")
-	fmt.Fprintln(w, "三个月未使用的药材及其当前库存:")
-	for _, g := range unuse {
-		fmt.Fprintf(w, "%s %v克 ", g.Name, g.Amount)
-	}
-	fmt.Fprintln(w)
+	jsonReply(w, map[string]interface{}{
+		"suggest": inuse,
+		"against": unuse,
+	})
 }

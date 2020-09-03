a9aa1d23177cc0f32c975babac20eb17c4e41e40
xiangrong.fang <xiangrong.fang@sensethink.com>
2020-09-03 13:18:28 +0800

    实现采购建议

--- src/api_invstat.go
+++ src/api_invstat.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 	"storekeeper/db"
+	"strconv"
 )
 
 func apiInvStat(w http.ResponseWriter, r *http.Request) {
@@ -16,9 +18,32 @@ func apiInvStat(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Unauthorized", http.StatusUnauthorized)
 		return
 	}
+	act, _ := strconv.Atoi(r.URL.Query().Get("act"))
 	inuse, unuse := db.AnalyzeGoodsUsage()
-	jsonReply(w, map[string]interface{}{
-		"suggest": inuse,
-		"against": unuse,
-	})
+	if act == 0 {
+		jsonReply(w, map[string]interface{}{
+			"suggest": inuse,
+			"against": unuse,
+		})
+		return
+	}
+	id := db.SetBill(db.Bill{Type: 1, User: uid})
+	var rx string
+	for _, u := range inuse {
+		rx += fmt.Sprintf("%v %v ", u.Name, u.Amount)
+	}
+	ps := db.GetPSItems(rx)
+	for _, p := range ps {
+		if len(p.Items) == 1 && p.Weight > 0 {
+			db.SetBillItem(db.BillItem{
+				BomID:     id,
+				Cost:      p.Items[0].Cost,
+				GoodsID:   p.Items[0].ID,
+				GoodsName: p.Items[0].Name,
+				Memo:      p.Memo,
+				Request:   p.Weight,
+			}, 0)
+		}
+	}
+	jsonReply(w, map[string]interface{}{"id": id})
 }

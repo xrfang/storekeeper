8117204d0a18b9f604a0837f40891f526927e327
xrfang <xrfang@gmail.com>
2020-09-24 21:36:32 +0800

    实现盘点锁定

--- src/h_chkin.go
+++ src/h_chkin.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 	"strconv"
 
@@ -31,6 +32,15 @@ func chkInEdit(w http.ResponseWriter, r *http.Request) {
 	uid := db.CheckToken(getCookie(r, "token"))
 	if uid == 0 {
 		http.Redirect(w, r, "/login", http.StatusTemporaryRedirect)
+		return
+	}
+	if db.InventoryInProgress() {
+		http.Error(w, "当前有未结束的盘点", http.StatusConflict)
+		return
+	}
+	//_probe参数表示客户端使用jquery测试本页面是否可以跳转，如果盘点进行中则不允许访问
+	if r.URL.Query().Get("_probe") != "" {
+		fmt.Fprintln(w, "OK") //可以继续访问
 		return
 	}
 	id, _ := strconv.Atoi(r.URL.Path[7:])

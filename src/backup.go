96930435b1918fd6c346f211434543cdd7363431
xrfang <xrfang@gmail.com>
2020-10-03 07:32:51 +0800

    WIP: backup db

--- src/backup.go
+++ src/backup.go
@@ -2,15 +2,36 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"os"
+	"path/filepath"
 	"time"
 
 	"github.com/robfig/cron/v3"
 )
 
+func cp(src, dst string) {
+	f, err := os.Open(src)
+	assert(err)
+	defer f.Close()
+	g, err := os.Create(dst)
+	assert(err)
+	defer func() { assert(g.Close()) }()
+	_, err = io.Copy(g, f)
+	assert(err)
+}
+
 func doBackup() {
-	L.Log("TODO: backup database")
-	bak := fmt.Sprintf("%s/herbs.db.%s", cf.bkupDir, time.Now().Format("20060102150405"))
-	fmt.Printf("copy %s => %s\n", cf.DBFile, bak)
+	tag := time.Now().Format("2006-01-02_1504")
+	bak := fmt.Sprintf("%s/%s.%s", cf.bkupDir, filepath.Base(cf.DBFile), tag)
+	L.Dbg("doBackup => %s", bak)
+	defer func() {
+		if e := recover(); e != nil {
+			L.Err("doBackup: %v", e)
+		}
+	}()
+	cp(cf.DBFile, bak)
+	fmt.Println("TODO: clean up old backup files and duplicates")
 }
 
 func startBackup() {

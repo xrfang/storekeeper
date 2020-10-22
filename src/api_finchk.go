16b55f530904148c1f14873d6defc8630438fd07
xiangrong.fang <xiangrong.fang@sensethink.com>
2020-10-22 08:47:37 +0800

    WIP: 会计API

---
+++ src/api_finchk.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"net/http"
+	"storekeeper/db"
+)
+
+func apiFinChk(w http.ResponseWriter, r *http.Request) {
+	defer func() {
+		if e := recover(); e != nil {
+			httpError(w, e)
+		}
+	}()
+	uid := db.CheckToken(getCookie(r, "token"))
+	if uid == 0 {
+		http.Error(w, "Unauthorized", http.StatusUnauthorized)
+		return
+	}
+	jsonReply(w, map[string]interface{}{})
+}

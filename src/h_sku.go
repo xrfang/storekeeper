d1b20dc7ed975bd7b8376b7bad679fd7aefc2662
xrfang <xrfang@gmail.com>
2022-09-06 08:45:55 +0800

    盘点时允许访问货品管理界面

--- src/h_sku.go
+++ src/h_sku.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 
 	"storekeeper/db"
@@ -16,15 +15,6 @@ func sku(w http.ResponseWriter, r *http.Request) {
 	uid := db.CheckToken(getCookie(r, "token"))
 	if uid == 0 {
 		http.Redirect(w, r, "/login", http.StatusTemporaryRedirect)
-		return
-	}
-	if db.InventoryWIP() != 0 {
-		http.Error(w, "当前有未结束的盘点", http.StatusConflict)
-		return
-	}
-	//_probe参数表示客户端使用jquery测试本页面是否可以跳转，如果盘点进行中则不允许访问
-	if r.URL.Query().Get("_probe") != "" {
-		fmt.Fprintln(w, "OK") //可以继续访问
 		return
 	}
 	switch r.Method {
